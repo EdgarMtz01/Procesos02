3b10f360991a422248ee605c307b7e7386c4910b
EdgarMtz01 <61388616+EdgarMtz01@users.noreply.github.com>
2020-02-26 22:22:58 -0600

    Second commit

--- Registro.cpp
+++ Registro.cpp
@@ -5,17 +5,17 @@ using namespace std;
 struct persona{
 	char nombre[20];
 	int edad;
-}persona1;
+}persona2;
 
 
 int main(){
 	cout<<"Nombre: ";
-	cin.getline(persona1.nombre,20,'\n');  //guarda cadena de caracteres
+	cin.getline(persona2.nombre,20,'\n');  //guarda cadena de caracteres
 	cout<<"Edad: ";
-	cin>>persona1.edad;
+	cin>>persona2.edad;
 	
-	cout<<"Nombre: "<<persona1.nombre<<endl;
-	cout<<"Edad: "<<persona1.edad<<endl;
+	cout<<"Nombre: "<<persona2.nombre<<endl;
+	cout<<"Edad: "<<persona2.edad<<endl;
 	
 	getch();
 	return 0;
